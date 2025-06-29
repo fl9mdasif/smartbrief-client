9c27364534afb8352929bb2c85d56eff23941c69
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2025-06-30 01:58:26 +0600

    add live server url to cliet

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://smart-breaf-alxexdbar-md-asifs-projects.vercel.app/api/v1/",
+  baseUrl: "https://smart-breaf-alxexdbar-md-asifs-projects.vercel.app/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
